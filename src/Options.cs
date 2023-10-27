89e6325d4b66b514c9a30933c4207a4bbca5db94
Dark-Gran <filip.randak@gmail.com>
2023-10-27 11:28:56 +0200

    First commit

---
+++ src/Options.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Menu.Remix.MixedUI;
+using RWCustom;
+using UnityEngine;
+
+namespace KarmaAppetite
+{
+
+    public class OptionsMenu : OptionInterface
+    {
+
+        public readonly Configurable<bool> testCheckBox;
+
+        public OptionsMenu(KarmaAppetite.Plugin plugin)
+        {
+            this.testCheckBox = this.config.Bind<bool>("KarmaAppetite_TestCheckBox", true, new ConfigurableInfo(null));
+        }
+
+        public override void Initialize()
+        {
+            OpTab opTab = new OpTab(this, "Default Canvas");
+            this.Tabs = new OpTab[]
+            {
+                opTab
+            };
+            OpContainer opContainer = new OpContainer(new Vector2(0f, 0f));
+            opTab.AddItems(new UIelement[]
+            {
+                opContainer
+            });
+            UIelement[] array = new UIelement[]
+            {
+                new OpCheckBox(this.testCheckBox, 20f, 550f),
+                new OpLabel(50f, 550f, "Test Check Box", false),
+            };
+            opTab.AddItems(array);
+        }
+
+        public override void Update()
+        {
+            base.Update();
+        }
+
+    }
+}
---
+++ src/Plugin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using BepInEx;
+using On;
+using UnityEngine;
+using SlugBase.Features;
+using static SlugBase.Features.FeatureTypes;
+using MonoMod.RuntimeDetour;
+
+namespace KarmaAppetite
+{
+
+    [BepInPlugin(MOD_ID, "Karma Appetite", "2.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        private const string MOD_ID = "darkgran.karmaappetite";
+
+
+        //-------CONFIG MENU-------
+        private OptionsMenu optionsMenuInstance;
+        private bool initialized;
+
+        public void RainWorld_OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+        {
+            orig.Invoke(self);
+            if (this.initialized)
+            {
+                return;
+            }
+            this.initialized = true;
+            this.optionsMenuInstance = new OptionsMenu(this);
+            try
+            {
+                MachineConnector.SetRegisteredOI(MOD_ID, this.optionsMenuInstance);
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(string.Format("Karma Appetite Options: Hook_OnModsInit options failed init error {0}{1}", this.optionsMenuInstance, ex));
+                base.Logger.LogError(ex);
+                base.Logger.LogMessage("OOPS");
+            }
+        }
+
+        //-------APPLY HOOKS-------
+        public void OnEnable()
+        {
+            On.RainWorld.OnModsInit += Extras.WrapInit(LoadResources);
+            On.RainWorld.OnModsInit += new On.RainWorld.hook_OnModsInit(this.RainWorld_OnModsInit);
+        }
+
+        private void LoadResources(RainWorld rainWorld)
+        {
+
+        }
+
+        //-------IMPLEMENT-------
+    }
+
+}
