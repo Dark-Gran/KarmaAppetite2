5b81295096030929fd29442eee326df3492b20e7
Dark-Gran <filip.randak@gmail.com>
2023-10-27 17:27:18 +0200

    saint-like hair contextual (like original)

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -2,6 +2,7 @@
 using BepInEx;
 using On;
 using IL;
+using RWCustom;
 using UnityEngine;
 using SlugBase.Features;
 using static SlugBase.Features.FeatureTypes;
@@ -64,8 +65,36 @@ namespace KarmaAppetite
         private void hook_DrawSprites(On.PlayerGraphics.orig_DrawSprites orig, PlayerGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
         {
             orig.Invoke(self, sLeaser, rCam, timeStacker, camPos);
-            float num = (self.player.bodyMode == Player.BodyModeIndex.Stand && self.player.input[0].x != 0) || self.player.bodyMode == Player.BodyModeIndex.Crawl ? (self.player.bodyMode == Player.BodyModeIndex.Crawl ? 7 : 6) : 0;
-            sLeaser.sprites[3].element = Futile.atlasManager.GetElementWithName("HeadB" + num.ToString());
+
+            //Saint-like hair
+            
+            int hair_num = 0;
+
+            if (self.player.sleepCurlUp > 0f)
+            {
+                hair_num = Custom.IntClamp((int)Mathf.Lerp((float)7, 4f, self.player.sleepCurlUp), 0, 8);
+            }
+            else if (self.owner.room != null && self.owner.EffectiveRoomGravity != 0f)
+            {
+                if (self.player.Consious)
+                {
+                    if ((self.player.bodyMode == Player.BodyModeIndex.Stand && self.player.input[0].x != 0) || self.player.bodyMode == Player.BodyModeIndex.Crawl)
+                    {
+                        hair_num = self.player.bodyMode == Player.BodyModeIndex.Crawl ? 7 : 6;
+                    }
+                    else
+                    {
+                        Vector2 vector = Vector2.Lerp(self.drawPositions[0, 1], self.drawPositions[0, 0], timeStacker);
+                        Vector2 vector2 = Vector2.Lerp(self.drawPositions[1, 1], self.drawPositions[1, 0], timeStacker);
+                        Vector2 vector3 = Vector2.Lerp(self.head.lastPos, self.head.pos, timeStacker);
+                        float num = Custom.AimFromOneVectorToAnother(Vector2.Lerp(vector2, vector, 0.5f), vector3);
+                        hair_num = Mathf.RoundToInt(Mathf.Abs(num / 360f * 34f));
+                    }
+                }
+            }
+
+            sLeaser.sprites[3].element = Futile.atlasManager.GetElementWithName("HeadB" + hair_num.ToString());
+            
         }
 
     }
